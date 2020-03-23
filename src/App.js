4f2195c7b4fd790c87c6d101b5d3949324adea7d
Stefan Bettaglio <sbettaglio@gmail.com>
2020-03-23 09:24:27 -0400

    button displays dice roll

--- src/App.js
+++ src/App.js
@@ -1,8 +1,23 @@
 import React, { Component } from 'react'
 import HelloWorld from './components/HelloWorld'
 
-const App = () => {
-  return <HelloWorld />
-}
+export class App extends Component {
+  state = {
+    dice: Math.round(Math.random() * 6),
+  }
+  rollDice = () => {
+    this.setState({
+      dice: Math.ceil(Math.random() * 6),
+    })
+  }
 
+  render() {
+    return (
+      <>
+        <p>{this.state.dice}</p>
+        <button onClick={this.rollDice}>Roll Dice</button>
+      </>
+    )
+  }
+}
 export default App
